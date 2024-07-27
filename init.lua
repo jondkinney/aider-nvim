1ed4089f496c794dc57c69f893dd05803d9bc9ed
Jon Kinney <jonkinney@gmail.com>
2024-07-26 21:08:09 -0500

    Remove keymap

--- init.lua
+++ init.lua
@@ -1,11 +1,8 @@
 require("aider").setup({
     -- Optionally override default settings
-    window_type = "vsplit",  -- Can be "vsplit", "hsplit", or "float"
+    window_type = "vsplit", -- Can be "vsplit", "hsplit", or "float"
     float_config = {
         width = 0.8,
         height = 0.8,
-    }
+    },
 })
-
--- Optional: Add a keymap to open Aider
-vim.api.nvim_set_keymap('n', '<leader>a', ':Aider<CR>', { noremap = true, silent = true })
